28c4f5d6582b664c4d935cfc8879ded94852fb5d
Jarrett <jarrett21@gmail.com>
2018-10-25 16:40:11 -0600

    experimental getMsg function implemented

--- src/JDBC_Connection.java
+++ src/JDBC_Connection.java
@@ -2,6 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
 
 public class JDBC_Connection { // class for connecting and reusable functions involving connections
 	public static Connection connect() throws SQLException{
@@ -19,8 +21,21 @@ public class JDBC_Connection { // class for connecting and reusable functions in
 		
 	}
 	
-	public static ResultSet getMsg(String usr) { // recieves unread messages
+	public static void getMsg(String usr, Connection conn) { // recieves unread messages
 		ResultSet query = null;
-		return query;
+		String sql = String.format("select * from inbox where email = '%s' and seen = 'n'", usr);
+		try {
+			Statement stmt = conn.createStatement();
+			query = stmt.executeQuery(sql);
+			while (query.next()) {
+				Timestamp time = query.getTimestamp(2);
+				String sender = query.getString(3);
+				String content = query.getString(4);
+				int rno = query.getInt(5);
+				System.out.println(String.format("From: %s Ride: %d [%s] %s\n", sender, rno, time.toString(), content));
+			}
+		} catch (Exception e) {
+			System.out.println("Connection to server failed");
+		}
 	}
 }
