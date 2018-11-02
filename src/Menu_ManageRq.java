c6626423e612db22ca6ccd1a7530e85b1d0b0483
Nicholas Serrano <34073804+nickroxcks@users.noreply.github.com>
2018-11-02 14:30:56 -0600

    added constructor to run the program

--- src/Menu_ManageRq.java
+++ src/Menu_ManageRq.java
@@ -1,4 +1,9 @@
+import java.sql.Connection;
+import java.util.Scanner;
 
 public class Menu_ManageRq {
-
+	private String usr;
+	Menu_ManageRq(String usr, Scanner scanner, Connection conn) {
+		this.usr = usr;
+	}
 }
