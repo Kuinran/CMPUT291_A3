e04c41e7f637b721383b14b4d4913a08c814be19
michael Huang <michaelhc@gmail.com>
2018-11-03 19:59:55 -0600

    implement message user

--- src/Menu_ManageRq.java
+++ src/Menu_ManageRq.java
@@ -30,10 +30,15 @@ public class Menu_ManageRq {
 	}
 	
 	private void search(Scanner scanner, Connection conn) throws SQLException {
-		System.out.println("Enter a city or location code");
+		System.out.println("Enter a city or location code of the pickup location");
 		String location = scanner.next().toLowerCase();
 		int i = parseUlocation(location);
-		
+		if (i == 0) {
+			System.out.println("Invalid input, please try again");
+			search(scanner, conn);
+		}
+		ListSearch(i, location, scanner, conn);
+		//message user
 	}
 	
 	private int parseUlocation(String location)throws SQLException {//check if input is lcode or city
@@ -74,8 +79,78 @@ public class Menu_ManageRq {
 		return 0;	//0 if no match
 	}
 	
+	private void ListSearch(int i, String location, Scanner scanner, Connection conn) throws SQLException {
+		List<String> count = new ArrayList<>();
+		String lcodesql = "select rid, rdate, pickup, dropoff, amount from requests where pickup = ?";
+		String citysql = "select rid, rdate, pickup, dropoff, amount from requests, location where location.lcode = pickup and city = ?";
+		
+		if (i == 1) {
+			PreparedStatement pstmt = conn.prepareStatement(lcodesql);
+			pstmt.setString(1, location);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				String rid = Integer.toString(rs.getInt("rid"));
+				String date = rs.getString("rdate");
+				String pickup = rs.getString("pickup");
+				String dropoff = rs.getString("dropoff");
+				String amt = Integer.toString(rs.getInt("amount"));
+				count.add(rid + "\t" + date + "\t" + pickup + "\t" + dropoff + "\t" + amt + "\n");
+			}
+			
+		}else if (i == 2) {
+			PreparedStatement pstmt = conn.prepareStatement(citysql);
+				pstmt.setString(1, location);
+				ResultSet rs = pstmt.executeQuery();
+				while(rs.next()) {
+					String rid = Integer.toString(rs.getInt("rid"));
+					String date = rs.getString("rdate");
+					String pickup = rs.getString("pickup");
+					String dropoff = rs.getString("dropoff");
+					String amt = Integer.toString(rs.getInt("amount"));
+					count.add(rid + "\t" + date + "\t" + pickup + "\t" + dropoff + "\t" + amt + "\n");
+				}
+			}
+		int s = count.size();
+		String List[] = new String[s];
+		for (int j = 0; j < count.size(); j ++) {
+			List[j] = count.get(j);
+		}
+		if (s >= 5) {
+			printList(List, List.length);
+		}else {
+			String all[][] = new String[s%5][];
+			for (int j = 0; j < s%5; j++) {
+				all[j] = UpdateList(List, j);
+			}
+			printList(all[0], 5);
+			int k = 1;
+			System.out.println("Please input 'n' for next page or anything else to continue.");
+			String input = scanner.next().toLowerCase();
+			while (input.equals("n") && k < s%5) {
+				printList(all[k], all[k].length);
+				System.out.println("Please type n for next page or anything else to continue.");
+				input = scanner.next().toLowerCase();
+				k++;
+			}
+		}
+	}
+	
+	private String[] UpdateList(String list[], int mod) {
+		String newString[] = new String[5];
+		for (int i = mod; i < mod + 5; i ++) {
+			newString[i] = list[i];
+		}
+		return newString;
+	}
+	
+	private void printList(String list[], int l) {
+		for (int i = 0; i < l; i++) {
+			System.out.println(list[i]);
+		}
+	}
+	
 	private void delete(String usr, Scanner scanner, Connection conn) throws SQLException {
-		//consider edge cases
+		
 		int ridList[] = listAll(usr);
 		System.out.println("Select one ID of request you wish to delete.");
 		int id = scanner.nextInt();
@@ -89,7 +164,7 @@ public class Menu_ManageRq {
 
 	}
 	
-	private int[] listAll(String usr)throws SQLException {
+	private int[] listAll(String usr){
 		List<Integer> count = new ArrayList<>();
 		String sql = "select rid, rdate, pickup, dropoff, amount from requests where requests.email = ?";
 		try (Connection conn = JDBC_Connection.connect();
@@ -116,7 +191,7 @@ public class Menu_ManageRq {
 		return ridList;
 	}
 	
-	private void DeleteRow(int id, Scanner scanner, Connection conn)throws SQLException {
+	private void DeleteRow(int id, Scanner scanner, Connection conn) throws SQLException {
 		
 		String sql = "delete from requests where rid = ? and email = ?";
 		
@@ -138,7 +213,7 @@ public class Menu_ManageRq {
 			delete(usr, scanner, conn);
 		} else if (del.equals("no")) {
 			mode(scanner, usr, conn);
-	}
+		}
 	}
 	
 }
