95858cf446f32e26f3276f8b12ccd778e02cb6c4
michael Huang <michaelhc@gmail.com>
2018-11-03 18:07:15 -0600

    fix search

--- src/Menu_ManageRq.java
+++ src/Menu_ManageRq.java
@@ -9,12 +9,12 @@ import java.util.List;
 
 public class Menu_ManageRq {
 	private String usr;
-	Menu_ManageRq(String usr, Scanner scanner, Connection conn) {
+	Menu_ManageRq(String usr, Scanner scanner, Connection conn) throws SQLException {
 		this.usr = usr;
 		mode(scanner, usr, conn);
 	}
 	
-	private void mode (Scanner scanner, String usr, Connection conn) {
+	private void mode (Scanner scanner, String usr, Connection conn) throws SQLException {
 		System.out.println("Enter 'Search', 'Delete', or 'exit'");
 		String input = scanner.next().toLowerCase();
 		if (input.equals("search")) {
@@ -29,14 +29,14 @@ public class Menu_ManageRq {
 		}
 	}
 	
-	private void search(Scanner scanner, Connection conn) {
+	private void search(Scanner scanner, Connection conn) throws SQLException {
 		System.out.println("Enter a city or location code");
 		String location = scanner.next().toLowerCase();
 		int i = parseUlocation(location);
 		
 	}
 	
-	private int parseUlocation(String location) {//check if input is lcode or city
+	private int parseUlocation(String location)throws SQLException {//check if input is lcode or city
 		List<String> loccode = new ArrayList<>();
 		List<String> loccity = new ArrayList<>();
 		String code = "select lcode from locations";
@@ -74,37 +74,22 @@ public class Menu_ManageRq {
 		return 0;	//0 if no match
 	}
 	
-	private void delete(String usr, Scanner scanner, Connection conn) {
+	private void delete(String usr, Scanner scanner, Connection conn) throws SQLException {
 		//consider edge cases
 		int ridList[] = listAll(usr);
 		System.out.println("Select one ID of request you wish to delete.");
 		int id = scanner.nextInt();
-		
-//		String sql = "delete from requests where rid = ? and email = ?";
-		//
-//		try(//Connection conn = JDBC_Connection.connect();
-//				PreparedStatement pstmt = conn.prepareStatement(sql)){
-//			pstmt.setInt(1, id);
-//			pstmt.setString(2, usr);
-//			pstmt.executeUpdate();
-//			
-//		} catch (SQLException e){
-//			System.out.println(e.getMessage());	
-//		}
-		System.out.println("Would you like to delete another request? enter yes or no.");
-		String del = scanner.next().toLowerCase();
-		while (!del.equals("yes") || !del.equals("no")) {
-			System.out.println("please enter yes or no.");
-			del = scanner.next().toLowerCase();
+		for (int i = 0; i < ridList.length; i++) {
+			if (id == ridList[i]) {
+				DeleteRow(id, scanner, conn);
+			}
 		}
-		if (del.equals("yes")) {
-			delete(usr, scanner, conn);
-		} else if (del.equals("no")) {
-			mode(scanner, usr, conn);
-		}
+		System.out.println("Invalid rid, please try again.");
+		delete(usr, scanner, conn);
+
 	}
 	
-	private int[] listAll(String usr) {
+	private int[] listAll(String usr)throws SQLException {
 		List<Integer> count = new ArrayList<>();
 		String sql = "select rid, rdate, pickup, dropoff, amount from requests where requests.email = ?";
 		try (Connection conn = JDBC_Connection.connect();
@@ -124,7 +109,6 @@ public class Menu_ManageRq {
 		}  catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-		//int i = count.get(count.size()-1);
 		int ridList[] = new int[count.size()];
 		for (int i = 0; i < count.size(); i++) {
 			ridList[i] = count.get(i);
@@ -132,12 +116,11 @@ public class Menu_ManageRq {
 		return ridList;
 	}
 	
-	private void DeleteRow() {
+	private void DeleteRow(int id, Scanner scanner, Connection conn)throws SQLException {
 		
 		String sql = "delete from requests where rid = ? and email = ?";
 		
-		try(Connection conn = JDBC_Connection.connect();
-				PreparedStatement pstmt = conn.prepareStatement(sql)){
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)){
 			pstmt.setInt(1, id);
 			pstmt.setString(2, usr);
 			pstmt.executeUpdate();
@@ -145,7 +128,17 @@ public class Menu_ManageRq {
 		} catch (SQLException e){
 			System.out.println(e.getMessage());	
 		}
+		System.out.println("Would you like to delete another request? enter yes or no.");
+		String del = scanner.next().toLowerCase();
+		while (!del.equals("yes") || !del.equals("no")) {
+			System.out.println("please enter yes or no.");
+			del = scanner.next().toLowerCase();
+		}
+		if (del.equals("yes")) {
+			delete(usr, scanner, conn);
+		} else if (del.equals("no")) {
+			mode(scanner, usr, conn);
 	}
-	
+	}
 	
 }
