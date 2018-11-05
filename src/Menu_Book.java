82750623cda30d5b0f688c2aa0ddb4a0239837c7
Nicholas Serrano <34073804+nickroxcks@users.noreply.github.com>
2018-11-04 18:43:37 -0700

    cancel function finished

--- src/Menu_Book.java
+++ src/Menu_Book.java
@@ -43,6 +43,37 @@ private String usr;
 		
 	}
 	public void cancel_booking(String usr,Scanner scanner, Connection conn) {
+		//bookings(bno, email, rno, cost, seats, pickup, dropoff)
+		List<String> membersemail = new ArrayList<>();
+		List<Integer> rnolist = new ArrayList<>();
+		System.out.println(usr + " Select a booking you would like to cancel(press corresponding number):\n");
+		String sql = "select b.bno,b.email,b.rno,b.cost,b.seats,b.pickup,b.dropoff "
+				+ "from bookings b, rides r where r.driver = ? and r.rno = b.rno";
+		String del = "delete from bookings where rno = ?";
+		try {
+		PreparedStatement pst = conn.prepareStatement(sql);
+		pst.setString(1, usr);
+		ResultSet rs1 = pst.executeQuery();
+		int counter = 1;
+		while(rs1.next()) {
+		System.out.println(counter + "- bno: " + rs1.getString(1) + " members email: " + rs1.getString(2) + " rno: " + rs1.getString(3) + 
+				" cost: " + rs1.getString(4) + " seats: " + rs1.getString(5) + " pickup: " + rs1.getString(6) + " dropoff: " + rs1.getString(7));
+		membersemail.add(rs1.getString(2));
+		rnolist.add(rs1.getInt(3));
+		counter++;
 		
+		}
+		String a = scanner.next();
+		int answer = Integer.parseInt(a);
+		PreparedStatement pst2 = conn.prepareStatement(del);
+		pst2.setInt(1, rnolist.get(answer-1));
+		pst2.executeUpdate();
+		System.out.println("rno "+ rnolist.get(answer-1) + " Deleted");
+		}
+		
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 }
